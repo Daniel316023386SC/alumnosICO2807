d2a7f1bab21621d96403b1bd4c9c053e05f44185
Daniel316023386SC <danielsanchez316@aragon.unam.mx>
2022-03-24 18:03:39 -0600

    Subiendo tarea

---
+++ DANIELSANCHEZCORTES.php
@@ -0,0 +1,7 @@
+<?php include ("header.php"); ?>
+
+    <body style="background: linear-gradient(to right, #182848, #4b6cb7);">
+        <head>
+            <h1 class="center">DANIEL SANCHEZ CORTES</h1>
+        </head>
+    </body>
